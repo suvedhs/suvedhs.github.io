dd1a1a9dc89e1c509447b16cc68c787dbab6c76a
suvedhs <suvedhs@gmail.com>
2018-12-15 01:27:30 -0600

    Updates

---
+++ precache-manifest.8c96b8dbd23c847c0cb6237e44a652ae.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "fec2bc48a9a70c202ceb",
+    "url": "/static/js/main.fec2bc48.chunk.js"
+  },
+  {
+    "revision": "82dafdb578ba27673946",
+    "url": "/static/js/1.82dafdb5.chunk.js"
+  },
+  {
+    "revision": "fec2bc48a9a70c202ceb",
+    "url": "/static/css/main.3c30b5bf.chunk.css"
+  },
+  {
+    "revision": "8ae95ff1a859dab6b080a620da24f3db",
+    "url": "/index.html"
+  }
+];
